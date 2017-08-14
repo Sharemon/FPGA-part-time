34943ec40f0e8ba21312ffca817738c0bf3d9b22
roy.pan <sharemon@163.com>
2017-08-14 22:22:39 +0800

    rm

--- PCIE_simulation/dma_performance_demo/pcie_7x_0_example/pcie_7x_0_example.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto 9331886148ed48cda4af7330e03049c0 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip -L xpm --snapshot board_behav xil_defaultlib.board xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- PCIE_simulation/dma_performance_demo/pcie_7x_0_example/pcie_7x_0_example.srcs/sources_1/imports/pcie_7x_0/bmd/BMD_128.v
+++
@@ -1,4 +0,0 @@
-`define BMD_128 1
-`define BMD_RX_ENGINE BMD_128_RX_ENGINE
-`define BMD_TX_ENGINE BMD_128_TX_ENGINE
-`define PCIE2_0 1 
